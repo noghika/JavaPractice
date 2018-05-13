5d2899949f8634eb6193b23271edde6e8f093641
hikaru <nayda.nh@gmail.com>
2018-05-13 17:02:14 +0900

    add practice1.java

---
+++ practice1.java
@@ -0,0 +1,16 @@
+/*画面に文字を出力するコード
+hikapon
+hikapon
+
+*/
+class practice1
+{
+    public static void main(String[] args)
+    {
+        // 16
+        System.out.println(0x6);
+        System.out.println(0x14);
+        System.out.println(0xd);
+
+    }
+}
