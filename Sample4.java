a0e68794addfd96c8e38e156c321bbfebd2a7f05
hikaru <nayda.nh@gmail.com>
2018-05-17 22:07:08 +0900

    add Sample4 & training1

---
+++ Sample4.java
@@ -0,0 +1,17 @@
+class Sample4
+{
+    public static void main(String[] args)
+    {
+        int num1 = 4;
+        int num2 = 7;
+        
+        System.out.println("num1とnum2にいろいろな演算を行います。");
+
+        System.out.println("num1+num2は" + (num1+num2) + "です。");
+        System.out.println("num1-num2は" + (num1-num2) + "です。");
+        System.out.println("num1*num2は" + (num1*num2) + "です。");
+        System.out.println("num1/num2は" + (num1/num2) + "です。");
+        System.out.println("num1%num2は" + (num1%num2) + "です。");
+
+    }
+}   
